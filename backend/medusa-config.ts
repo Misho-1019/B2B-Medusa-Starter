fc0ab8f9c095262db242f4a4d89fd929d4f869e1
Misho1019 <mikailtorres99@gmail.com>
2025-08-14 08:44:46 +0300

    Update my config file

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -1,11 +1,14 @@
-import { QUOTE_MODULE } from "./src/modules/quote";
-import { APPROVAL_MODULE } from "./src/modules/approval";
-import { COMPANY_MODULE } from "./src/modules/company";
-import { loadEnv, defineConfig, Modules } from "@medusajs/framework/utils";
+// backend/medusa-config.ts
+import { QUOTE_MODULE } from "./src/modules/quote"
+import { APPROVAL_MODULE } from "./src/modules/approval"
+import { COMPANY_MODULE } from "./src/modules/company"
+import { loadEnv, defineConfig, Modules } from "@medusajs/framework/utils"
 
-loadEnv(process.env.NODE_ENV!, process.cwd());
+loadEnv(process.env.NODE_ENV!, process.cwd())
 
-module.exports = defineConfig({
+const isProd = process.env.NODE_ENV === "production"
+
+export default defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     http: {
@@ -17,20 +20,78 @@ module.exports = defineConfig({
     },
   },
   modules: {
-    [COMPANY_MODULE]: {
-      resolve: "./modules/company",
-    },
-    [QUOTE_MODULE]: {
-      resolve: "./modules/quote",
-    },
-    [APPROVAL_MODULE]: {
-      resolve: "./modules/approval",
-    },
-    [Modules.CACHE]: {
-      resolve: "@medusajs/medusa/cache-inmemory",
-    },
-    [Modules.WORKFLOW_ENGINE]: {
-      resolve: "@medusajs/medusa/workflow-engine-inmemory",
-    },
+    [COMPANY_MODULE]: { resolve: "./modules/company" },
+    [QUOTE_MODULE]: { resolve: "./modules/quote" },
+    [APPROVAL_MODULE]: { resolve: "./modules/approval" },
+
+    // Cache
+    ...(isProd
+      ? {
+          [Modules.CACHE]: {
+            resolve: "@medusajs/medusa/cache-redis",
+            options: { redisUrl: process.env.REDIS_URL },
+          },
+        }
+      : {
+          [Modules.CACHE]: {
+            resolve: "@medusajs/medusa/cache-inmemory",
+          },
+        }),
+
+    // Workflow engine
+    ...(isProd
+      ? {
+          [Modules.WORKFLOW_ENGINE]: {
+            resolve: "@medusajs/medusa/workflow-engine-redis",
+            options: { redis: { url: process.env.REDIS_URL } },
+          },
+        }
+      : {
+          [Modules.WORKFLOW_ENGINE]: {
+            resolve: "@medusajs/medusa/workflow-engine-inmemory",
+          },
+        }),
+
+    // File storage (new module system)
+    ...(isProd
+      ? {
+          [Modules.FILE]: {
+            resolve: "@medusajs/modules-file",
+            options: {
+              providers: [
+                {
+                  resolve: "@medusajs/modules-file-s3",
+                  id: "s3",
+                  options: {
+                    file_url: process.env.S3_FILE_URL,
+                    access_key_id: process.env.S3_ACCESS_KEY_ID,
+                    secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+                    region: process.env.S3_REGION || "us-east-1",
+                    bucket: process.env.S3_BUCKET,
+                    endpoint: process.env.S3_ENDPOINT,
+                    additional_client_config: {
+                      forcePathStyle:
+                        process.env.S3_FORCE_PATH_STYLE === "true",
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        }
+      : {}),
+
+    // Search (Meili)
+    ...(isProd
+      ? {
+          ["search"]: {
+            resolve: "@medusajs/medusa/search-meilisearch",
+            options: {
+              host: process.env.MEILI_HOST,
+              api_key: process.env.MEILI_MASTER_KEY,
+            },
+          },
+        }
+      : {}),
   },
-});
+})
